1b61bf25922914fefc5855f0a53486c8edf94521
rui <chanruiheng2004@gmail.com>
2026-09-07 10:36:12 +0800

    submit

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -10,7 +10,7 @@
     <!-- Add/modify code in the following to have a dropdown list 
         which lets the user to select either to login with username or email -->
     <select v-model="loginOption">
-        <option value="username">Username login</option>
+        <option selected value="username">Username login</option>
         <option value="email">Email login</option>
     </select>
     
